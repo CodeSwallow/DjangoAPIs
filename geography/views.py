d0e3f36fabf36d863e121532b5c78c24af11a47e
Isai Ramirez <isai.ramirez.stamaria15@gmail.com>
2022-04-22 11:44:39 -0500

    Added ReadOnlyModelViewSets in geography for Country, City, Continent

--- geography/views.py
+++ geography/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from geography.models import Country, Continent, City
+from geography.serializers import CountrySerializer, ContinentSerializer, CitySerializer
 
 # Create your views here.
+
+
+class CountryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name')
+        if name is not None:
+            self.queryset = self.queryset.filter(name__contains=name)
+        return self.queryset
+
+
+class ContinentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Continent.objects.all()
+    serializer_class = ContinentSerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name')
+        if name is not None:
+            self.queryset = self.queryset.filter(name__contains=name)
+        return self.queryset
+
+
+class CityViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name')
+        if name is not None:
+            self.queryset = self.queryset.filter(name__contains=name)
+        return self.queryset
+
